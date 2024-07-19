a6da1ce676ae782f5675a905ed4adfdaf8d40446
abmoger <57551169+abmoger@users.noreply.github.com>
2024-07-19 16:35:38 -0400

    Initial Set Up

---
+++ functionality.js
@@ -0,0 +1,69 @@
+function mouseOnImageOne() {
+    var picOne = document.getElementById("cBeam");
+    picOne.src = "Materials/Images/C-Beam2.png";
+}
+
+function mouseOffImageOne() {
+    var picOne = document.getElementById("cBeam");
+    picOne.src = "Materials/Images/C-Beam.png";
+}
+
+
+
+function mouseOnImageTwo() {
+    var picTwo = document.getElementById("menses");
+    picTwo.src = "Materials/Images/Menses2.png";
+}
+
+function mouseOffImageTwo() {
+    var picTwo = document.getElementById("menses");
+    picTwo.src = "Materials/Images/MensesGame.png";
+}
+
+
+
+function mouseOnImageThree() {
+    var picThree = document.getElementById("lost");
+    picThree.src = "Materials/Images/Lost_In_Translation2.png";
+}
+
+function mouseOffImageThree() {
+    var picThree = document.getElementById("lost");
+    picThree.src = "Materials/Images/Lost_In_Translation1.png";
+}
+
+
+
+function mouseOnImageFour() {
+    var picFour = document.getElementById("shape");
+    picFour.src = "Materials/Images/Shapes2.png";
+}
+
+function mouseOffImageFour() {
+    var picFour = document.getElementById("shape");
+    picFour.src = "Materials/Images/Shapes1.png";
+}
+
+
+
+function mouseOnImageFive() {
+    var picFive = document.getElementById("him");
+    picFive.src = "Materials/Images/Him2.png";
+}
+
+function mouseOffImageFive() {
+    var picFive = document.getElementById("him");
+    picFive.src = "Materials/Images/Him1.png";
+}
+
+
+
+function mouseOnImageSix() {
+    var picFive = document.getElementById("sapience");
+    picFive.src = "Materials/Images/Sapience2.png";
+}
+
+function mouseOffImageSix() {
+    var picFive = document.getElementById("sapience");
+    picFive.src = "Materials/Images/Sapience1.png";
+}
